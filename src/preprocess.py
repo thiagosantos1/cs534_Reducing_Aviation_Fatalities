c301e53c70489e61554cf94530e3d6d0c26404b3
Theodore (Alex) Evans <taevans@cs.unm.edu>
2019-11-19 18:34:55 -0500

    tune preprocess to not be file specific

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,15 +2,19 @@
 
 import pandas as pd
 import numpy as np
+import sys
 
-data = pd.read_csv('train.csv')
-print(data.shape)
-uq = data[['crew','experiment']].drop_duplicates()
-print(uq)
-for ix,row in uq.iterrows():
-    crew=row['crew']
-    experiment=row['experiment']
-    f1=data[data['crew']==crew]
-    f2=f1[f1['experiment']==experiment]
-    f2.sort_values('time').to_csv('train_{:02}_{}.csv'.format(crew, experiment))
+for i in range(1, len(sys.argv)):
+    data = pd.read_csv(sys.argv[i])
+    head = sys.argv[i].rsplit('.', 1)[0]
+    print(data.shape)
+    uq = data[['crew', 'experiment']].drop_duplicates()
+    for ix, row in uq.iterrows():
+        crew = row['crew']
+        experiment = row['experiment']
+        filename = '{}_{:02}_{}.csv'.format(head, crew, experiment)
+        print(filename)
+        f1 = data[data['crew'] == crew]
+        f2 = f1[f1['experiment'] == experiment]
+        f2.sort_values('time').to_csv(filename)
 
