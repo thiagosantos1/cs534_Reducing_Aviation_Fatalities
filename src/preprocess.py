9b8cddb23b682d5c8f02430f4b0d5c00fd0f98e9
Theodore (Alex) Evans <varmfskii@gmail.com>
2019-11-20 11:38:31 -0500

    separate the two signal sets

--- src/preprocess.py
+++ src/preprocess.py
@@ -8,13 +8,15 @@ for i in range(1, len(sys.argv)):
     data = pd.read_csv(sys.argv[i])
     head = sys.argv[i].rsplit('.', 1)[0]
     print(data.shape)
-    uq = data[['crew', 'experiment']].drop_duplicates()
+    uq = data[['crew', 'experiment', 'seat']].drop_duplicates()
     for ix, row in uq.iterrows():
         crew = row['crew']
         experiment = row['experiment']
-        filename = '{}_{:02}_{}.csv'.format(head, crew, experiment)
+        seat = row['seat']
+        filename = '{}_{:02}_{}_{}.csv'.format(head, crew, experiment, seat)
         print(filename)
         f1 = data[data['crew'] == crew]
         f2 = f1[f1['experiment'] == experiment]
-        f2.sort_values('time').to_csv(filename)
+        f3 = f2[f2['seat'] == seat]
+        f3.sort_values('time').to_csv(filename)
 
