3161034cfbd981e101b02930c7c83951fae493c2
Theodore (Alex) Evans <varmfskii@gmail.com>
2019-12-06 14:33:03 -0500

    cubic spline interpolation

--- src/int_test.py
+++ src/int_test.py
@@ -5,20 +5,35 @@ import matplotlib.pyplot as pp
 import numpy as np
 import pandas as pd
 import sys
-from tools import interpolate, lognorm2norm
+from tools import int_cubic, int_spline, lognorm2norm
 
 def get_pulse(ecgin):
     out = ecg.ecg(signal=ecgin, sampling_rate=256, show=False)
     return out
 
 
+data0 = np.array(range(10))+.5
+ts1 = (np.array(range(100))+.5)/10
+pp.plot(data0, data0*2, label='original')
+data1=int_cubic(data0*2, data0, ts1)
+pp.plot(ts1, data1+.1, label='cubic')
+data1=int_spline(data0*2, data0, ts1)
+pp.plot(ts1, data1+.2, label='spline')
+pp.legend()
+pp.show()
 data = pd.read_csv(sys.argv[1])
 pulse = get_pulse(data['ecg'])
 # pp.plot(pulse['heart_rate_ts'], pulse['heart_rate'])
-pulse2 = interpolate(pulse['heart_rate'], pulse['heart_rate_ts'], data['time'])
-norm = lognorm2norm(pulse2)
-pp.plot(data['time'], norm)
+pulse1 = int_cubic(pulse['heart_rate'], pulse['heart_rate_ts'], data['time'])
+pulse2 = int_spline(pulse['heart_rate'], pulse['heart_rate_ts'], data['time'])
+pp.plot(pulse['heart_rate_ts'], pulse['heart_rate'], label='original')
+pp.plot(data['time'], pulse1, label='cubic')
+pp.plot(data['time'], pulse2, label='spline')
+pp.legend()
 pp.show()
-pp.hist(norm, bins=20)
+norm1 = lognorm2norm(pulse1)
+norm2 = lognorm2norm(pulse2)
+pp.hist(norm1, bins=20)
+pp.hist(norm2, bins=20)
 pp.show()
                  
--- src/tools.py
+++ src/tools.py
@@ -1,7 +1,34 @@
 import numpy as np
 
+ep = 0.0001
 
-def interpolate(in_vec, in_ts, out_ts):
+def slope(val, fn):
+    vec = np.empty(4)
+    vec[0] = 0.0
+    vec[1] = 1.0
+    vec[2] = 2.0*val
+    vec[3] = 3.0*val*val
+    return np.dot(vec, fn)
+
+
+def delta(val, fn):
+    vec1 = np.empty(4)
+    vec1[0] = 1.0
+    vec1[1] = (val+ep)
+    vec1[2] = vec1[1]*(val+ep)
+    vec1[3] = vec1[2]*(val+ep)
+    y1 = np.dot(vec1, fn)
+    vec2 = np.empty(4)
+    vec2[0] = 1.0
+    vec2[1] = (val-ep)
+    vec2[2] = vec2[1]*(val-ep)
+    vec2[3] = vec2[2]*(val-ep)
+    y2 = np.dot(vec2, fn)
+    return (y1-y2)/(2.0*ep)
+
+
+def int_cubic(in_vec, in_ts, out_ts):
+    # interpolation using a local 3rd degree polynomial
     in_len=in_ts.shape[0]
     out_len=out_ts.shape[0]
     in_pow = np.empty((in_len, 4))
@@ -15,11 +42,7 @@ def interpolate(in_vec, in_ts, out_ts):
     hi = in_vec.max()
     out_vec = np.empty(out_len)
     win_mat = in_pow[0:4,:]
-    win_inv = np.linalg.inv(win_mat)
-    win_fn = np.dot(win_inv, in_vec[0:4])
-    for i in range(4):
-        vec = in_pow[i,:]
-        # print(in_vec[i], np.dot(vec, win_fn))
+    win_fn = np.linalg.solve(win_mat, in_vec[0:4])
                        
     wix = 0
     for i in range(out_len):
@@ -27,8 +50,61 @@ def interpolate(in_vec, in_ts, out_ts):
             while wix+4 < in_len and out_ts[i] > in_ts[wix+2]:
                 wix += 1
             win_mat = in_pow[wix:wix+4,:]
-            win_inv = np.linalg.inv(win_mat)
-            win_fn = np.dot(win_inv, in_vec[wix:wix+4])
+            win_fn = np.linalg.solve(win_mat, in_vec[wix:wix+4])
+        v = np.dot(out_pow[i,:], win_fn)
+        if v<lo:
+            out_vec[i] = lo
+        elif v>hi:
+            out_vec[i] = hi
+        else:
+            out_vec[i] = v
+    return out_vec
+
+
+def int_spline(in_vec, in_ts, out_ts):
+    # interpolation using a cubic spline
+    in_len=in_ts.shape[0]
+    out_len=out_ts.shape[0]
+
+    din_vec = np.empty(in_len)
+    din_vec[0] = (in_vec[1]-in_vec[0])/(in_ts[1]-in_ts[0])
+    din_vec[1:in_len-1] = (in_vec[2:]-in_vec[:in_len-2])/(in_ts[2:]-in_ts[:in_len-2])
+    din_vec[in_len-1] = (in_vec[in_len-1]-in_vec[in_len-2])/(in_ts[in_len-1]-in_ts[in_len-2])
+    in_pow = np.empty((in_len, 4))
+    out_pow = np.empty((out_len, 4))
+    in_pow[:,0] = np.ones(in_len)
+    out_pow[:,0] = np.ones(out_len)
+    for i in range(1,4):
+        in_pow[:,i] = in_pow[:,i-1]*in_ts
+        out_pow[:,i] = out_pow[:,i-1]*out_ts
+
+    din_pow = np.empty((in_len, 4))
+    din_pow[:,0] = np.zeros(in_len)
+    din_pow[:,1] = np.ones(in_len)
+    din_pow[:,2] = 2*in_ts
+    din_pow[:,3] = 3*in_ts*in_ts
+
+    lo = in_vec.min()
+    hi = in_vec.max()
+    out_vec = np.empty(out_len)
+    win_mat = np.empty((4, 4))
+    win_mat[0:2,:] = in_pow[0:2,:]
+    win_mat[2:4,:] = din_pow[0:2,:]
+    win_vec = np.empty((4,))
+    win_vec[0:2] = in_vec[0:2]
+    win_vec[2:4] = din_vec[0:2]
+    win_fn = np.linalg.solve(win_mat, win_vec)
+          
+    wix = 0
+    for i in range(out_len):
+        if wix+2 < in_len and out_ts[i] > in_ts[wix+1]:
+            while wix+2 < in_len and out_ts[i] > in_ts[wix+1]:
+                wix += 1
+            win_mat[0:2,:] = in_pow[wix:wix+2,:]
+            win_mat[2:4,:] = din_pow[wix:wix+2,:]
+            win_vec[0:2] = in_vec[wix:wix+2]
+            win_vec[2:4] = din_vec[wix:wix+2]
+            win_fn = np.linalg.solve(win_mat, win_vec)
         v = np.dot(out_pow[i,:], win_fn)
         if v<lo:
             out_vec[i] = lo
